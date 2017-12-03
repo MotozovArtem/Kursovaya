cb96dadb53321df4f822a8d4c2b44e66330aaee0
Rienel <mav199824@yandex.ru>
2017-12-03 15:59:13 +0000

    Удален loginsys. Коммит перед слиянием

--- loginsys/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoginsisConfig(AppConfig):
-    name = 'loginsys'
--- loginsys/models.py
+++
@@ -1,6 +0,0 @@
-# from django.db import models
-#
-# # Create your models here.
-# class Register(models.Model):
-#     username=models.Model(models.CharField)
-#     password=models.Model(models.CharField)
